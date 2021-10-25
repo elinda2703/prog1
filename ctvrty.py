4d3839fc36c8559dc6d52d65fbc8a0cc39708998
elinda <ela.pospechova@gmail.com>
2021-10-25 15:36:59 +0200

    nnnnnn

---
+++ ctvrty.py
@@ -0,0 +1,20 @@
+from random import randrange
+
+
+
+x=randrange(10)
+
+
+while x:
+    y=int(input("zadej číslo"))
+    if (x<y):
+        print ("moc velké")
+    elif(x>y):
+        print("moc malé")
+    else:
+        print("jjjjj")
+        break    
+
+print("nnnnnn")
+
+

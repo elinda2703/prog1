9cce54b42ec7fc626d555ac367de25683a7a5357
elinda <ela.pospechova@gmail.com>
2021-11-04 14:10:51 +0100

    almost done

--- ukol1.py
+++ ukol1.py
@@ -1,6 +1,7 @@
-from turtle import forward,left,right,penup,pendown,speed,exitonclick,circle,setposition
+from turtle import forward,left,right,penup,pendown,speed,exitonclick,circle,setposition,color,pensize
+from math import sqrt
 #speed(1)
-def piskvorky(a,b,d):
+def hraci_pole(a,b,d):
     for j in range (b):
         for i in range (a):
             for k in range (4):
@@ -12,33 +13,49 @@ def piskvorky(a,b,d):
         right(90)
         forward(d)
         right(90)
-    for l in range(int(a*b/2)):
-        penup()
+
+def kolecko(a,b,d):
+    penup()
+    while(True):
         x1=int(input("hráči 1, zadej x"))
         y1=int(input("hráči 1, zadej y"))
-        setposition((x1-1)*d+d/2,(y1-1)*d+d/6)
-        pendown()
-        circle(d/3)
+        if (x1>a or y1>b or x1<=0 or y1<=0):
+            print ("neplatný vstup")
+        else:
+            break
+    setposition((x1-1)*d+d/2,(y1-1)*d+d/6)
+    pendown()
+    color('blue')
+    circle(d/3)
+
+def krizek(a,b,d):
         penup()
-        x2=int(input("hráči 2, zadej x"))
-        y2=int(input("hráči 2, zadej y"))
+        while(True):
+            x2=int(input("hráči 2, zadej x"))
+            y2=int(input("hráči 2, zadej y"))
+            if (x2>a or y2>b or x2<=0 or y2<=0):
+                print ("neplatný vstup")
+            else:
+                break
         setposition((x2-1)*d+d/6,(y2-1)*d+d/6)
         pendown()
+        color('red')
         left(45)
-        forward(2/3*d*1.41421)
+        forward(2/3*d*sqrt(2))
         penup()
         right(135)
         forward(2*d/3)
         pendown()
         right(135)
-        forward(2/3*d*1.41421)
+        forward(2/3*d*sqrt(2))
         right(135)
+
+def piskvorky(a,b,d):
+    hraci_pole(a,b,d)
+    pensize(3)
+    for l in range(int(a*b/2)):
+        kolecko(a,b,d)
+        krizek(a,b,d)
     if (a*b%2==1):
-        penup()
-        x1=int(input("hráči 1, zadej x"))
-        y1=int(input("hráči 1, zadej y"))
-        setposition((x1-1)*d+d/2,(y1-1)*d+d/6)
-        pendown()
-        circle(d/3)
-        penup()
-piskvorky (2,2,50)
+        kolecko(a,b,d)
+piskvorky (int(input("zadej počet řádků")),int(input("zadej počet sloupců")),int(input("zadej velikost pole")))
